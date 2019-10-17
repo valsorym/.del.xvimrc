55612f6463f36234aad35cb68c9948cdd4d3a19e
valsorym <valsorym.e@gmail.com>
2019-10-17 11:14:43 +0300

    Update

--- bin/vim.sh
+++ bin/vim.sh
@@ -8,6 +8,7 @@ cd $SCRIPT_DIR/../
 BASE_DIR=`pwd -P`
 
 # Copy the basic settings.
+rm -Rf $HOME/.vim $HOME/.vimrc $HOME/.gvimrc
 mkdir -p $HOME/.vim/bundle
 cp -Rf $BASE_DIR/vimrc/init.vim $HOME/.vimrc
 cp -Rf $BASE_DIR/vimrc/ginit.vim $HOME/.gvimrc
